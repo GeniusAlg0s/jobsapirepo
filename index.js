6f09fb35305cbc84d1701088e45835e3bbd322c8
PatWil <patrick.williams@tlgcohort.com>
2021-04-22 08:42:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+///packages
+
+const express = require("express");
+
+const { getJobs } = require("./services");
+
+const server = express();
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`sever listen on port: ${PORT}`);
+}); /// port to listen on
+
+server.get("/jobs", (req, res) => {
+  //const tech= req.query.tech
+// localhost:3000/jobs?tech=python
+// localhost:3000/jobs?tech=java
+// localhost:3000/jobs?tech=C++
+  const { tech } = req.query;
+
+
+
+  if (tech === undefined) {
+    return res.status(400).send({ error: "Tech query parameter is undefined" });
+  }
+
+  //   res.send(jobs);
+  // getJobs(tech).then((jobs) => res.send(jobs[0]));
+
+  getJobs(tech).then((jobs) => res.send(jobs[0]));
+
+  
+});
+
+server.get("/", (req, res) => {
+  console.log("home route");
+});
